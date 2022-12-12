b27e052acfd10ad79e6de36afe0500f52f6aa924
Ricky~DMS <59780719+chefpatatdev@users.noreply.github.com>
2022-12-12 22:37:13 +0100

    Update Monitor_CENTRAL.ino

--- main/Monitor_CENTRAL.ino
+++ main/Monitor_CENTRAL.ino
@@ -1,42 +1,86 @@
-//PERF
+//CENTRAL
+
 
 #include <ArduinoBLE.h>
 
-String pidValues ="24.5;0.0095;655"; //format Kp;Ki;Kd
-
-BLEService customService("19B10000-E8F2-537E-4F6C-D104768A1214"); 
-BLEStringCharacteristic pidValuesBLE("2A19", BLERead | BLENotify,20);
-
+double PIDout_value;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  // configure the button pin as input
-
   // initialize the Bluetooth® Low Energy hardware
-  BLE.setLocalName("PID_Monitor");
-  BLE.setAdvertisedService(customService); // add the service UUID
-  customService.addCharacteristic(pidValuesBLE); // add the battery level characteristic
-  BLE.addService(customService); // Add the battery service
-  pidValuesBLE.writeValue(pidValues); 
-  BLE.advertise();
+  BLE.begin();
 
+  Serial.println("Bluetooth® Low Energy Central - PIDmonitor");
 
+  // start scanning for peripherals
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
 }
 
 void loop() {
-  BLEDevice central = BLE.central();
-  while(Serial.available()){
-    pidValues = Serial.readString();  
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
 
-    if(central){
-      pidValuesBLE.writeValue(pidValues);
-    }else{
-      Serial.println("Couldnt send to robot");
+  if (peripheral) {
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    if (peripheral.localName() != "PID_Monitor") {
+      return;
     }
-  }
 
+    BLE.stopScan();
+
+    readValues(peripheral);
+
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  }
 }
 
+void readValues(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
 
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+
+
+  BLECharacteristic PIDoutput = peripheral.characteristic("2A19");
+
+  if (!PIDoutput) {
+    Serial.println("Peripheral does not a characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+
+  while (peripheral.connected()) {
+    PIDoutput.read();
+      float floatValue;
+      PIDoutput.readValue( &floatValue, 4 ); //read the value with bluetooth
+      Serial.println( floatValue );
+
+  }
+
+  Serial.println("Peripheral disconnected");
+}
